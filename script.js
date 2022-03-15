5d8f9cab36ea6f31a9b142abd62219c3a780e3a7
Adrian Furmanek <adrian.furm@gmail.com>
2022-03-15 22:00:41 +0100

    components, styling

---
+++ script.js
@@ -0,0 +1,25 @@
+const playerScoreElement = document.getElementById('playerScore');
+const playerChoiceElement = document.getElementById('playerChoice'); 
+const computerScoreElement = document.getElementById('computerScore');
+const computerChoiceElement = document.getElementById('computerChoice');
+const resultText = document.getElementById('resultText');
+const playerRock = document.getElementById('playerRock');
+const playerPaper = document.getElementById('playerPaper');
+const playerScissors = document.getElementById('playerScissors');
+const playerLizard = document.getElementById('playerLizard');
+const playerSpock = document.getElementById('playerSpock');
+const computerRock = document.getElementById('computerRock');
+const computerPaper = document.getElementById('computerPaper');
+const computerScissors = document.getElementById('computerScissors');
+const computerLizard = document.getElementById('computerLizard');
+const computerSpock = document.getElementById('computerSpock');
+const allGameIcons = document.querySelectorAll('.far');
+
+const choices = {
+  rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
+  paper: { name: 'Paper', defeats: ['rock', 'spock'] },
+  scissors: { name: 'Scissors', defeats: ['paper', 'lizard'] },
+  lizard: { name: 'Lizard', defeats: ['paper', 'spock'] },
+  spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
+};
+
